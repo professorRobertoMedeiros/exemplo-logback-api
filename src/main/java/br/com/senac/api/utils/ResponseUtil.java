20a54de2f1b4172329cc559f21585f9fcf76d658
ROBERTO FERMINO MEDEIROS <roberto.medeiros@prof.sc.senac.br>
2024-09-17 20:59:07 -0300

    revert commit master

--- src/main/java/br/com/senac/api/utils/ResponseUtil.java
+++ src/main/java/br/com/senac/api/utils/ResponseUtil.java
@@ -8,8 +8,7 @@ public class ResponseUtil {
         Map<String, Object> response = new HashMap<>();
         response.put("messages", message);
 
-        System.out.println("Teste GIT 2");
-        System.out.println("Teste GIT 3");
+        System.out.println("Teste GIT");
 
         return response;
     }
